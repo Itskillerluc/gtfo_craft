ee2c897fc0e7da18a3d8e5742e4073b06ee30a9b
Itskillerluc <lvgraef@zeelandnet.nl>
2023-07-15 16:34:13 +0200

    it now stays

--- src/main/java/io/github/itskillerluc/gtfo_craft/entity/EntityFogRepeller.java
+++ src/main/java/io/github/itskillerluc/gtfo_craft/entity/EntityFogRepeller.java
@@ -1,13 +1,22 @@
 package io.github.itskillerluc.gtfo_craft.entity;
 
 import io.github.itskillerluc.gtfo_craft.registry.BlockRegistry;
+import io.github.itskillerluc.gtfo_craft.registry.ItemRegistry;
+import io.github.itskillerluc.gtfo_craft.tileentity.TileEntityFog;
+import net.minecraft.block.material.Material;
+import net.minecraft.block.state.IBlockState;
 import net.minecraft.entity.EntityLivingBase;
+import net.minecraft.entity.projectile.EntityArrow;
 import net.minecraft.entity.projectile.EntityThrowable;
+import net.minecraft.init.SoundEvents;
+import net.minecraft.item.ItemStack;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.MathHelper;
 import net.minecraft.util.math.RayTraceResult;
 import net.minecraft.world.World;
+import org.apache.logging.log4j.LogManager;
 
-public class EntityFogRepeller extends EntityThrowable {
+public class EntityFogRepeller extends EntityArrow {
     public static final int DIAMETER = 3;
 
     public EntityFogRepeller(World worldIn) {
@@ -22,9 +31,15 @@ public class EntityFogRepeller extends EntityThrowable {
         super(worldIn, throwerIn);
     }
 
+
     @Override
-    protected void onImpact(RayTraceResult result) {
-        if (result.typeOfHit != RayTraceResult.Type.MISS){
+    public void setDead() {
+        super.setDead();
+    }
+
+    @Override
+    protected void onHit(RayTraceResult result) {
+        if (result.typeOfHit != RayTraceResult.Type.MISS) {
             BlockPos pos = result.getBlockPos();
             if (pos == null) {
                 pos = result.entityHit.getPosition();
@@ -58,10 +73,30 @@ public class EntityFogRepeller extends EntityThrowable {
                     }
                 }
             }
+            BlockPos blockpos = result.getBlockPos();
+
+            this.motionX = (double)((float)(result.hitVec.x - this.posX));
+            this.motionY = (double)((float)(result.hitVec.y - this.posY));
+            this.motionZ = (double)((float)(result.hitVec.z - this.posZ));
+            float f2 = MathHelper.sqrt(this.motionX * this.motionX + this.motionY * this.motionY + this.motionZ * this.motionZ);
+            this.posX -= this.motionX / (double)f2 * 0.05000000074505806D;
+            this.posY -= this.motionY / (double)f2 * 0.05000000074505806D;
+            this.posZ -= this.motionZ / (double)f2 * 0.05000000074505806D;
+            this.inGround = true;
+            this.arrowShake = 7;
+            this.setIsCritical(false);
+            pickupStatus = PickupStatus.DISALLOWED;
         }
-        if (!this.world.isRemote)
-        {
-            this.world.setEntityState(this, (byte)3);
+    }
+    @Override
+    protected ItemStack getArrowStack() {
+        return new ItemStack(ItemRegistry.FOG_REPELLER);
+    }
+
+    @Override
+    public void onUpdate() {
+        super.onUpdate();
+        if (this.timeInGround >= TileEntityFog.TOTAL_TIME) {
             this.setDead();
         }
     }
