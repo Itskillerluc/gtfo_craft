a5477bc792536a8436cf222a7eea7a3b84aa522a
Itskillerluc <lvgraef@zeelandnet.nl>
2023-11-09 16:43:30 +0100

    fixed not working outside dev env

--- src/main/java/io/github/itskillerluc/gtfo_craft/GtfoCraft.java
+++ src/main/java/io/github/itskillerluc/gtfo_craft/GtfoCraft.java
@@ -16,11 +16,11 @@ import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLServerStartingEvent;
 import org.apache.logging.log4j.Logger;
 
-@Mod(modid = GtfoCraft.MODID, name = GtfoCraft.NAME, version = GtfoCraft.VERSION)
+@Mod(modid = GtfoCraft.MODID, name = GtfoCraft.NAME, version = GtfoCraft.VERSION, dependencies = "required-after:geckolib3")
 public class GtfoCraft
 {
     public static final String MODID = "gtfo_craft";
-    public static final String NAME = "GTFO: Craft Protocol";
+    public static final String NAME = "GTFO Craft Protocol";
     public static final String VERSION = "1.0";
 
     @Mod.Instance(MODID)
