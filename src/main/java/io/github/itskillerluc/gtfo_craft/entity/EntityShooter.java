084e3304cda94da07592979e4867495000f2be24
Itskillerluc <lvgraef@zeelandnet.nl>
2024-01-07 21:03:10 +0100

    Shooter
    Added the shooter entity

--- src/main/java/io/github/itskillerluc/gtfo_craft/entity/EntityShooter.java
+++ src/main/java/io/github/itskillerluc/gtfo_craft/entity/EntityShooter.java
@@ -1,5 +1,7 @@
 package io.github.itskillerluc.gtfo_craft.entity;
 
+import io.github.itskillerluc.gtfo_craft.entity.ai.AnimatedRangedAttack;
+import io.github.itskillerluc.gtfo_craft.entity.ai.EntityAIRangedBurst;
 import io.github.itskillerluc.gtfo_craft.entity.ai.gtfoEntity;
 import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.entity.IRangedAttackMob;
@@ -13,6 +15,7 @@ import net.minecraft.network.datasync.DataSerializers;
 import net.minecraft.network.datasync.EntityDataManager;
 import net.minecraft.util.math.MathHelper;
 import net.minecraft.world.World;
+import software.bernie.geckolib3.core.AnimationState;
 import software.bernie.geckolib3.core.IAnimatable;
 import software.bernie.geckolib3.core.PlayState;
 import software.bernie.geckolib3.core.builder.AnimationBuilder;
@@ -24,9 +27,9 @@ import software.bernie.geckolib3.core.manager.AnimationFactory;
 
 public class EntityShooter extends ModEntity implements IAnimatable, IRangedAttackMob, gtfoEntity {
 
-    private static final AnimationBuilder SLEEP1 = new AnimationBuilder().addAnimation("sleep1", ILoopType.EDefaultLoopTypes.LOOP);
-    private static final AnimationBuilder SLEEP2 = new AnimationBuilder().addAnimation("sleep2", ILoopType.EDefaultLoopTypes.LOOP);
-    private static final AnimationBuilder SLEEP3 = new AnimationBuilder().addAnimation("sleep3", ILoopType.EDefaultLoopTypes.LOOP);
+    private static final AnimationBuilder SLEEP1 = new AnimationBuilder().addAnimation("default_sleeping", ILoopType.EDefaultLoopTypes.LOOP);
+    private static final AnimationBuilder SLEEP2 = new AnimationBuilder().addAnimation("sleeping_1", ILoopType.EDefaultLoopTypes.LOOP);
+    private static final AnimationBuilder SLEEP3 = new AnimationBuilder().addAnimation("sleeping_2", ILoopType.EDefaultLoopTypes.LOOP);
     private static final AnimationBuilder RUN = new AnimationBuilder().addAnimation("run", ILoopType.EDefaultLoopTypes.LOOP);
     private static final AnimationBuilder ATTACK = new AnimationBuilder().addAnimation("attack", ILoopType.EDefaultLoopTypes.LOOP);
     private static final AnimationBuilder SCREAM = new AnimationBuilder().addAnimation("scream", ILoopType.EDefaultLoopTypes.LOOP);
@@ -34,6 +37,12 @@ public class EntityShooter extends ModEntity implements IAnimatable, IRangedAtta
 
     private static final DataParameter<Boolean> ATTACKING =
             EntityDataManager.createKey(EntityShooter.class, DataSerializers.BOOLEAN);
+    private boolean isAttacking;
+    private int animCounter;
+    private final int animTickLength = 40;
+    private final int screamLength = 55;
+    private boolean isScreaming = false;
+    private int screamCounter;
 
     public EntityShooter(World worldIn) {
         super(worldIn);
@@ -86,7 +95,7 @@ public class EntityShooter extends ModEntity implements IAnimatable, IRangedAtta
         this.tasks.addTask(7, new EntityAIWanderAvoidWater(this, 1.0D));
         this.tasks.addTask(8, new EntityAIWatchClosest(this, EntityPlayer.class, 8.0F));
         this.tasks.addTask(8, new EntityAILookIdle(this));
-        this.tasks.addTask(4, new EntityAIAttackRanged(this, 1, 5, 20));
+        this.tasks.addTask(4, new AnimatedRangedAttack<>(this, 1, 60, 20));
 
         this.targetTasks.addTask(1, new EntityAIHurtByTarget(this, false));;
         this.targetTasks.addTask(2, new EntityAINearestAttackableTarget<>(this, EntityPlayer.class, true));
@@ -94,17 +103,17 @@ public class EntityShooter extends ModEntity implements IAnimatable, IRangedAtta
 
     @Override
     public AnimationBuilder getSleeping0() {
-        return null;
+        return SLEEP1;
     }
 
     @Override
     public AnimationBuilder getSleeping1() {
-        return null;
+        return SLEEP2;
     }
 
     @Override
     public AnimationBuilder getSleeping2() {
-        return null;
+        return SLEEP3;
     }
 
     @Override
@@ -135,10 +144,19 @@ public class EntityShooter extends ModEntity implements IAnimatable, IRangedAtta
         }
         return PlayState.STOP;
     }
+    private <E extends IAnimatable> PlayState screamPredicate(AnimationEvent<E> event) {
+        if (isScreaming) {
+            event.getController().setAnimation(SCREAM);
+            return PlayState.CONTINUE;
+        }
+        return PlayState.STOP;
+    }
+
     @Override
     public void registerControllers(AnimationData data) {
         data.addAnimationController(new AnimationController<>(this, "controller", 0, this::predicate));
         data.addAnimationController(new AnimationController<>(this, "attackController", 0, this::attackPredicate));
+        data.addAnimationController(new AnimationController(this, "screamController", 0, this::screamPredicate));
     }
 
     @Override
@@ -151,11 +169,44 @@ public class EntityShooter extends ModEntity implements IAnimatable, IRangedAtta
         entityPellet.shoot(d0, d1 + d3 * 0.20000000298023224D, d2, 1.6F, (float)(14 - this.world.getDifficulty().getDifficultyId() * 4));
         this.playSound(SoundEvents.ENTITY_SKELETON_SHOOT, 1.0F, 1.0F / (this.getRNG().nextFloat() * 0.4F + 0.8F));
         this.world.spawnEntity(entityPellet);
+        setAttacking(true);
+        isAttacking = true;
     }
 
     @Override
     public void setSwingingArms(boolean swingingArms) {
-        setAttacking(swingingArms);
+        if (swingingArms) {
+            freeze();
+        } else {
+            unfreeze();
+        }
         this.isSwingInProgress = swingingArms;
+    }
+
+    @Override
+    public void onUpdate() {
+        super.onUpdate();
+        if (isAttacking) {
+            animCounter++;
+
+            if (animCounter >= animTickLength) {
+                animCounter = 0;
+                setAttacking(false);
+                isAttacking = false;
+            }
+        }
+
+        if (isScreaming) {
+            screamCounter++;
+
+            if (screamCounter >= screamLength) {
+                screamCounter = 0;
+                isScreaming = false;
+            }
+        }
+
+        if (rand.nextFloat() < 0.004) {
+            isScreaming = true;
+        }
     }
 }
--- src/main/java/io/github/itskillerluc/gtfo_craft/entity/ai/AnimatedRangedAttack.java
+++ src/main/java/io/github/itskillerluc/gtfo_craft/entity/ai/AnimatedRangedAttack.java
@@ -1,15 +1,16 @@
 package io.github.itskillerluc.gtfo_craft.entity.ai;
 
-import io.github.itskillerluc.gtfo_craft.entity.ModEntity;
+import net.minecraft.entity.EntityCreature;
 import net.minecraft.entity.EntityLiving;
 import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.entity.IRangedAttackMob;
+import net.minecraft.entity.ai.EntityAIAttackRanged;
 import net.minecraft.entity.ai.EntityAIBase;
 import net.minecraft.util.math.MathHelper;
 
-public class EntityAIRangedBurst <T extends ModEntity & IRangedAttackMob> extends EntityAIBase {
+public class AnimatedRangedAttack<E extends EntityCreature & gtfoEntity & IRangedAttackMob> extends EntityAIBase {
     private final EntityLiving entityHost;
-    private final T rangedAttackEntityHost;
+    private final E rangedAttackEntityHost;
     private EntityLivingBase attackTarget;
     private int rangedAttackTime;
     private final double entityMoveSpeed;
@@ -18,31 +19,26 @@ public class EntityAIRangedBurst <T extends ModEntity & IRangedAttackMob> extend
     private final int maxRangedAttackTime;
     private final float attackRadius;
     private final float maxAttackDistance;
-    private final int burst;
-    private final int fullAttackDuration;
-    private int burstTime = 0;
-    private boolean shouldAttackNextTick = false;
-    private int bursted = 0;
 
-    public EntityAIRangedBurst(T attacker, double movespeed, int maxAttackTime, float maxAttackDistanceIn, int burst, int fullAttackDuration)
+
+    public AnimatedRangedAttack(E attacker, double movespeed, int maxAttackTime, float maxAttackDistanceIn)
     {
-        this(attacker, movespeed, maxAttackTime, maxAttackTime, maxAttackDistanceIn, burst, fullAttackDuration);
+        this(attacker, movespeed, maxAttackTime, maxAttackTime, maxAttackDistanceIn);
     }
 
-    public EntityAIRangedBurst(T attacker, double movespeed, int p_i1650_4_, int maxAttackTime, float maxAttackDistanceIn, int burst, int fullAttackDuration)
+    public AnimatedRangedAttack(E attacker, double movespeed, int p_i1650_4_, int maxAttackTime, float maxAttackDistanceIn)
     {
-        this.fullAttackDuration = fullAttackDuration;
         this.rangedAttackTime = -1;
-        this.burst = burst;
 
-        if (!(attacker instanceof EntityLivingBase))
+
+        if (attacker == null)
         {
             throw new IllegalArgumentException("ArrowAttackGoal requires Mob implements RangedAttackMob");
         }
         else
         {
             this.rangedAttackEntityHost = attacker;
-            this.entityHost = attacker;
+            this.entityHost = (EntityLiving)attacker;
             this.entityMoveSpeed = movespeed;
             this.attackIntervalMin = p_i1650_4_;
             this.maxRangedAttackTime = maxAttackTime;
@@ -63,7 +59,6 @@ public class EntityAIRangedBurst <T extends ModEntity & IRangedAttackMob> extend
         else
         {
             this.attackTarget = entitylivingbase;
-            rangedAttackEntityHost.freeze();
             return true;
         }
     }
@@ -75,16 +70,13 @@ public class EntityAIRangedBurst <T extends ModEntity & IRangedAttackMob> extend
 
     public void resetTask()
     {
-        ((IRangedAttackMob)entityHost).setSwingingArms(false);
         this.attackTarget = null;
         this.seeTime = 0;
         this.rangedAttackTime = -1;
-        rangedAttackEntityHost.unfreeze();
     }
 
     public void updateTask()
     {
-
         double d0 = this.entityHost.getDistanceSq(this.attackTarget.posX, this.attackTarget.getEntityBoundingBox().minY, this.attackTarget.posZ);
         boolean flag = this.entityHost.getEntitySenses().canSee(this.attackTarget);
 
@@ -107,34 +99,23 @@ public class EntityAIRangedBurst <T extends ModEntity & IRangedAttackMob> extend
         }
 
         this.entityHost.getLookHelper().setLookPositionWithEntity(this.attackTarget, 30.0F, 30.0F);
-        rangedAttackTime--;
-        burstTime++;
-        if ((shouldAttackNextTick && (burstTime % (fullAttackDuration / burst)) == 0) || this.rangedAttackTime == 0)
+
+        if (--this.rangedAttackTime == 0)
         {
             if (!flag)
             {
                 return;
             }
-            ((IRangedAttackMob)entityHost).setSwingingArms(true);
 
             float f = MathHelper.sqrt(d0) / this.attackRadius;
             float lvt_5_1_ = MathHelper.clamp(f, 0.1F, 1.0F);
             this.rangedAttackEntityHost.attackEntityWithRangedAttack(this.attackTarget, lvt_5_1_);
             this.rangedAttackTime = MathHelper.floor(f * (float)(this.maxRangedAttackTime - this.attackIntervalMin) + (float)this.attackIntervalMin);
-            shouldAttackNextTick = bursted++ < burst;
-            if (bursted > burst) {
-                bursted = 0;
-                shouldAttackNextTick = false;
-                burstTime = 0;
-            }
         }
         else if (this.rangedAttackTime < 0)
         {
             float f2 = MathHelper.sqrt(d0) / this.attackRadius;
-            shouldAttackNextTick = true;
             this.rangedAttackTime = MathHelper.floor(f2 * (float)(this.maxRangedAttackTime - this.attackIntervalMin) + (float)this.attackIntervalMin);
-        } else if (!shouldAttackNextTick) {
-            ((IRangedAttackMob)entityHost).setSwingingArms(false);
         }
     }
 }
